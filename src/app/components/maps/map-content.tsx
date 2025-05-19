7cc5d580d4f323eedf7fbaaabf55a0fa65bf0678
Suisan <yamashitamarin0725@gmail.com>
2025-05-19 13:38:03 +0900

    Update map-content.tsx

--- src/app/components/maps/map-content.tsx
+++ src/app/components/maps/map-content.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { AdvancedMarker, APIProvider, Map } from '@vis.gl/react-google-maps';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
 export const MapContent = () => {
   return (
@@ -8,9 +8,8 @@ export const MapContent = () => {
       <Map
         defaultZoom={15}
         defaultCenter={{ lat: 35.656, lng: 139.737 }}
-      >
-        <AdvancedMarker position={{ lat: 35.656, lng: 139.737 }} />
-      </Map>
+        mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID || ''}
+      />
     </APIProvider>
   );
 };
