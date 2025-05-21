83a7c9c12c36fc654e9528e21426df91c65a9b39
Suisan <yamashitamarin0725@gmail.com>
2025-05-21 20:56:06 +0900

    Update MapComponent.tsx
    元に戻す

--- src/app/MapComponent.tsx
+++ src/app/MapComponent.tsx
@@ -1,34 +1,21 @@
-'use client';
-
+// src/app/MapComponent.tsx
 import React from 'react';
-import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
+import { APIProvider, Map } from '@vis.gl/react-google-maps';
 
-type Place = {
-  place_id: string;
-  name: string;
-  geometry: { location: { lat: number; lng: number } };
-};
-
-type Props = {
-  places: Place[];
-  center: { lat: number; lng: number };
-};
-
-export const MapContent = ({ places, center }: Props) => {
+const MapComponent = () => {
   return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''} mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID || ''}>
-      <Map defaultZoom={15} defaultCenter={center} gestureHandling={'greedy'} disableDefaultUI={true} style={{ width: '100%', height: '400px' }}>
-        {places.map((place) => (
-          <Marker
-            key={place.place_id}
-            position={{
-              lat: place.geometry.location.lat,
-              lng: place.geometry.location.lng,
-            }}
-            title={place.name}
-          />
-        ))}
-      </Map>
+    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}>
+      <Map
+        style={{ width: '100vw', height: '100vh' }}
+        defaultCenter={{ lat: 22.54992, lng: 0 }}
+        defaultZoom={3}
+        gestureHandling={'greedy'}
+        disableDefaultUI={true}
+      />
     </APIProvider>
+  );
+};
+
+export default MapComponent;
   );
 };
