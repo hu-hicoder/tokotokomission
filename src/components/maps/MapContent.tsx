d8406cbf594b61559ec6a128f4381303de975a31
Suisan <yamashitamarin0725@gmail.com>
2025-05-23 00:49:10 +0900

    Update MapContent.tsx

--- src/components/maps/MapContent.tsx
+++ src/components/maps/MapContent.tsx
@@ -10,21 +10,21 @@ type Place = {
 };
 
 type Props = {
-  places: Place[];
-  center: { lat: number; lng: number };
+  places?: Place[]; // 取得場所がある場合も、無い場合も対応できるようにoptionalに
+  center?: { lat: number; lng: number }; // centerもoptionalに
 };
 
-export const MapContent = ({ places, center }: Props) => {
+export const MapContent = ({ places = [], center = { lat: 35.656, lng: 139.737 } }: Props) => {
   return (
     <APIProvider
       apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}
       mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID || ''}
     >
       <Map
-        key={`${center.lat}-${center.lng}`} // これでcenterが変わるたび再描画促進
+        key={`${center.lat}-${center.lng}`}
         center={center}
         zoom={15}
-        gestureHandling={'greedy'}
+        gestureHandling="greedy"
         disableDefaultUI={true}
         style={{ width: '100%', height: '400px' }}
       >
@@ -39,21 +39,6 @@ export const MapContent = ({ places, center }: Props) => {
           />
         ))}
       </Map>
-    </APIProvider>
-  );
-};
-'use client';
-
-import { APIProvider, Map } from '@vis.gl/react-google-maps';
-
-export const MapContent = () => {
-  return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
-      <Map
-        defaultZoom={15}
-        defaultCenter={{ lat: 35.656, lng: 139.737 }}
-        mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID || ''}
-      />
     </APIProvider>
   );
 };
