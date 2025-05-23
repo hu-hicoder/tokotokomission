0845df7d47adf95271e2e9dbdf354bf2f95b2a1e
suisan <b241651@hiroshima-u.ac.jp>
2025-05-23 17:44:40 +0900

    引数を取る作業

--- src/components/maps/MapContent.tsx
+++ src/components/maps/MapContent.tsx
@@ -2,35 +2,17 @@
 
 import React from 'react';
 import { APIProvider, Map, Marker } from '@vis.gl/react-google-maps';
-import { usePlacesContext } from '@/context/PlacesContext';
-
-
-type Place = {
-  place_id: string;
-  name: string;
-  geometry: { location: { lat: number; lng: number } };
-};
 
 type Props = {
-  places?: Place[];
-  center?: { lat: number; lng: number };
-};
+       places: Place[];
+       center: LatLag;
+     };
 
-<<<<<<< HEAD
-export const MapContent = () => {
-  const { places = [], center = { lat: 35.656, lng: 139.737 } } = usePlacesContext();
-
-  return (
-    <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
-      <Map
-        key={`${center?.lat}-${center?.lng}`}
-=======
-export const MapContent = ({ places = [], center = { lat: 35.656, lng: 139.737 } }: Props) => {
+export const MapContent = ({ places, center }: Props) => {
   return (
     <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY || ''}>
       <Map
         key={`${center.lat}-${center.lng}`}
->>>>>>> c922943357fe1f9959fb96136ccf0d73529ded9e
         center={center}
         zoom={15}
         mapId={process.env.NEXT_PUBLIC_GOOGLE_MAPS_ID || ''}
